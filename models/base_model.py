929ff4f655e9acac00f2aa06f88f3abc5bf5a4f3
Seid_Hamid <seidhamid200@gmail.com>
2022-11-28 10:52:35 -0800

    pycodestyle fix

--- models/base_model.py
+++ models/base_model.py
@@ -21,40 +21,36 @@ class BaseModel:
             for key, val in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, val)
-                    self.created_at = datetime.strptime
-                    (kwargs["created_at"], dtm)
-                    if hasattr(self, 'updated_at')
-                    and type(self.updated_at) is str:
-                        self.updated_at = datetime.strptime
-                        (kwargs["updated_at"], dtm)
-                    else:
-                        self.id = str(uuid.uuid4())
-                        self.created_at = datetime.now()
-                        self.updated_at = self.created_at
-                        models.storage.new(self)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(
+                    kwargs["created_at"], dtm)
+            if hasattr(self, 'updated_at') and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], dtm)
 
-                        def __str__(self):
-                            """str representation"""
-                            return "[{:s}] ({:s}) {}".format
-                        (self.__class__.__name__, self.id,
-                         self.__dict__)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
-                        def save(self):
-                            """updates the public ins attr
-upd_at with the curren one"""
-                            self.updated_at = datetime.now()
-                            models.storage.save()
+    def __str__(self):
+        """string represent BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
-                            def to_dict(self):
-                                """ returns a dic containing keys
-and values of the instance"""
-                                n_dict = self.__dict__.copy()
-                                if "created_at" in n_dict:
-                                    n_dict["created_at"] = n_dict
-                                    ["created_at"].strftime(dtm)
-                                    if "updated_at" in n_dict:
-                                        n_dict["updated_at"] = n_dict
-                                        ["updated_at"].strftime(dtm)
-                                        n_dict["__class__"] = self.
-                                        __class__.__name__
-                                        return n_dict
+    def save(self):
+        """updates the public instance att updated_at tocurrent date"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ return a dictionary with all key/value"""
+        n_dict = self.__dict__.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"].strftime(dtm)
+            if "updated_at" in n_dict:
+                n_dict["updated_at"] = n_dict["updated_at"].strftime(dtm)
+                n_dict["__class__"] = self.__class__.__name__
+                return n_dict
